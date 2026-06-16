3574cfce9bc4e590d31aef00f8d6a84191b6e230
Vishesh Gandhi <gandhivishesh222@gmail.com>
2026-06-16 23:22:42 +0530

    Add resolvers and schema index files for user and event management

---
+++ src/resolvers/index.js
@@ -0,0 +1,4 @@
+const userResolvers = require('./user.resolver');
+const eventResolvers = require('./event.resolver');
+
+module.exports = [userResolvers, eventResolvers];
---
+++ src/schema/index.js
@@ -0,0 +1,4 @@
+const userTypeDefs = require('./user.schema');
+const eventTypeDefs = require('./event.schema');
+
+module.exports = [userTypeDefs, eventTypeDefs];
